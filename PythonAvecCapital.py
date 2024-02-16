cedb03fde0086d14c018c77363c698567728591f
julienAvecScript <therealjennaloublack@gmail.com>
2024-02-16 16:11:14 -0500

    Updated robot to read parameters from "parameters.json"

--- PythonAvecCapital.py
+++ PythonAvecCapital.py
@@ -6,9 +6,10 @@ from coinbase.websocket import WSClient, WSClientConnectionClosedException, WSCl
 from coinbase.wallet.client import Client
 
 CONF_FILE_PATH = "config.json"
+PARA_FILE_PATH = "parameters.json"
 
 
-def load_configuration():
+def loadConfig():
     if not os.path.isfile(CONF_FILE_PATH):
         return {}
 
@@ -22,27 +23,43 @@ def load_configuration():
             "api_secret": conf["api_secret"]
         }
 
+def loadParameters():
+    if not os.path.isfile(PARA_FILE_PATH):
+        return {}
+
+    with open(PARA_FILE_PATH) as para_file:
+        para = json.load(para_file)
+
+        return para
+
 
 if __name__ == "__main__":
-    conf = load_configuration()
+    conf = loadConfig()
+    para = loadParameters()
     state = 0
     symbol = input("What symbol are you trading? ")
     high = 0
     low = 9999999999999999
     entry = 0
-    dipAmplitude = 0.02
-    recoveryAmplitude = 0.01
-    pumpAmplitude = 0.03
-    lossAmplitude = 0.01
     buyPrice = 0
     sellPrice = 0
 
     if not conf:
         print(
-            "Error: Could not find the configuration file (coinbase_cloud_api_key.json) under the 'conf' folder. Did you read the README?")
+            "Error: Could not find the configuration file (config.json). Did you read the README?")
+        exit()
+
+    if not para:
+        print(
+            "Error: Could not find the parameters file (parameters.json). Did you read the README?")
         exit()
 
     client = Client(conf["api_key"], conf["api_secret"])
+    dipAmplitude = float(para["dipAmplitude"])
+    recoveryAmplitude = float(para["recoveryAmplitude"])
+    pumpAmplitude = float(para["pumpAmplitude"])
+    lossAmplitude = float(para["lossAmplitude"])
+
     try:
         user = client.get_current_user()
         print(user)
@@ -64,32 +81,33 @@ if __name__ == "__main__":
         match state:
             case 0:
                 dip = entry * dipAmplitude
-                print("-------------------- " + str(spot) + " STATE " + str(state) + ", MAX PRICE " + str(entry - dip) + " @" + str(now))
+                print("-------------------- " + str(spot) + " WAITING FOR DROP TO " + str(entry - dip) + " @" + str(now))
                 if spot < entry - dip:
                     print("******************** ENTERED @" + str(entry - dip))
                     state = 1
             case 1:
                 recovery = low * recoveryAmplitude
-                print("-------------------- " + str(spot) + " STATE " + str(state) + ", MIN PRICE " + str(low + recovery) + " @" + str(now))
+                print("-------------------- " + str(spot) + " WAITING FOR RECOVERY TO " + str(low + recovery) + " @" + str(now))
                 if spot > low + recovery:
                     buyPrice = spot
                     print("******************** BOUGHT @" + str(buyPrice))
                     state = 2
             case 2:
                 pump = low * pumpAmplitude
-                print("-------------------- " + str(spot) + " STATE " + str(state) + ", MIN PRICE " + str(low + pump) + " @" + str(now))
+                print("-------------------- " + str(spot) + " WAITING FOR PUMP TO " + str(low + pump) + " @" + str(now))
                 if spot > low + pump:
                     print("******************** CONFIRMED @" + str(low + pump))
                     state = 3
             case 3:
                 loss = high * lossAmplitude
-                print("-------------------- " + str(spot) + " STATE " + str(state) + ", MAX PRICE " + str(high - loss) + " @" + str(now))
+                print("-------------------- " + str(spot) + " WAITING FOR DROP TO " + str(high - loss) + " @" + str(now))
                 if low + (low * pumpAmplitude) < spot < high - loss:
                     sellPrice = spot
                     print("******************** SOLD @" + str(sellPrice) + " & BOUGHT @" + str(buyPrice) + " FOR " + str(
                         sellPrice / buyPrice) + "%! RE-ENTRYING..." + " @" + str(now))
                     entry = spot
                     state = 0
+
     def on_message(msg):
         global entry
         global high
@@ -105,10 +123,10 @@ if __name__ == "__main__":
                     spot = float(spotRaw)
                     if spot < low:
                         low = spot
-                        print('******************** LO: ' + str(spot))
+                        print('******************** DISCOVERED NEW LOW @ ' + str(spot))
                     if spot > high:
                         high = spot
-                        print('******************** HI: ' + str(spot))
+                        print('******************** DISCOVERED NEW HIGH @ ' + str(spot))
                         if state == 0:
                             entry = spot
                     compute(spot)
